a624012e59bf38514c7d668c5b8d37ad6920bf46
Darius Cepulis <de.cepulis@gmail.com>
2020-10-30 13:37:58 -0500

    Initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+</script>
+
+<style>
+  :global(html, body) {
+    font-family: "Comic Sans MS", serif;
+    background-color: blue;
+    color: yellow;
+    text-align: center;
+  }
+  main {
+    min-width: 300px;
+    max-width: 500px;
+    padding: 0 1rem;
+    margin: 0 auto;
+  }
+</style>
+
+<Nav {segment} />
+
+<main>
+  <slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script>
+  import underconstruction from "./_underconstruction.gif";
+</script>
+
+<svelte:head>
+  <title>Kristin & Darius</title>
+</svelte:head>
+
+<main>
+  <h1>Kristin and Darius are finally getting married!</h1>
+  <p>Watch this space for updates.</p>
+  <p>
+    <img
+      src={underconstruction}
+      alt="This site is under construction"
+      width="300" />
+  </p>
+  <h2>Can't Wait to Hear The Latest?</h2>
+  <p><i>Subscribe to our newsletter!</i></p>
+  <form name="subscribe" method="POST" data-netlify="true">
+    <p><label>Your Name: <input type="text" name="name" /></label></p>
+    <p><label>Your Email: <input type="email" name="email" /></label></p>
+    <p>
+      <label>
+        Star Wars or Star Trek?:
+        <select name="scifi">
+          <option disabled selected value hidden>Choose Wisely...</option>
+          <option value="starwars">Star Wars</option>
+          <option value="startrek">Star Trek</option>
+        </select>
+      </label>
+    </p>
+    <p><button type="submit">Subscribe</button></p>
+  </form>
+</main>
