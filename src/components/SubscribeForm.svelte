4974db266301c5156b8e2178aa46d23c834db1f2
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-02-01 15:04:12 -0600

    Update sub form to match new style

--- src/components/SubscribeForm.svelte
+++ src/components/SubscribeForm.svelte
@@ -1,34 +1,6 @@
-<style>
-  form {
-    max-width: var(--content-width);
-    margin: 0 auto;
-
-    display: grid;
-    grid-template-columns: 150px 1fr;
-    gap: 1.4rem;
-    grid-gap: 1.4rem;
-    text-align: left;
-  }
-  @media (max-width: 500px) {
-    form {
-      text-align: center;
-      gap: 0.7rem;
-      grid-gap: 0.7rem;
-    }
-    form label,
-    form input,
-    form select {
-      grid-column: span 2;
-    }
-  }
-  form button[type="submit"] {
-    grid-column: span 2;
-    padding: 0.7rem;
-  }
-  form * {
-    font: inherit;
-  }
-</style>
+<script>
+  import Button from "./Button.svelte";
+</script>
 
 <form id="subscribe" name="subscribe" method="POST" data-netlify="true">
   <input type="hidden" id="formname" name="formname" value="subscribe" />
@@ -44,5 +16,45 @@
     <option value="starwars">Star Wars</option>
     <option value="startrek">Star Trek</option>
   </select>
-  <button type="submit">Subscribe</button>
+  <Button type="submit">Subscribe</Button>
 </form>
+
+<style>
+  form {
+    max-width: var(--content-width);
+    margin: 0 auto;
+
+    display: grid;
+    grid-template-columns: 150px 1fr;
+    gap: 1.4rem;
+    grid-gap: 1.4rem;
+    text-align: left;
+  }
+
+  form input,
+  form select {
+    background: none;
+    border: var(--border) solid white;
+    color: inherit;
+    font: inherit;
+    cursor: pointer;
+  }
+  @media (max-width: 500px) {
+    form {
+      text-align: center;
+      gap: 0.7rem;
+      grid-gap: 0.7rem;
+    }
+    form label,
+    form input,
+    form select {
+      grid-column: span 2;
+    }
+  }
+  form > :global(button[type="submit"]) {
+    grid-column: span 2;
+  }
+  form * {
+    font: inherit;
+  }
+</style>
