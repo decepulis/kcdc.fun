dcfdb90ca478989815f361886d84e574e8f4910f
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-05-01 11:20:45 -0500

    Stream test

---
+++ src/routes/test.svelte
@@ -0,0 +1,26 @@
+<p class="embed-container">
+<iframe
+      width="560"
+      height="315"
+      src="https://www.youtube-nocookie.com/embed/mOI-3BLRqus?controls=0"
+      title="The Wedding of Kristin Bradford and Darius Cepulis"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
+</p>
+
+<style>
+    .embed-container {
+    width: 100%;
+    padding-bottom: calc(100% * 0.5625);
+    position: relative;
+  }
+  .embed-container iframe {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+  }
+</style>
