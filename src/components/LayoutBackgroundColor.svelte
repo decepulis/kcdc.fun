9e0c9890753e3ff41b47d5fae667432e01ef2aec
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-10-19 11:02:51 -0500

    Cleanup: Autoprefixer, NPM Update, Remove Old Files

--- src/components/LayoutBackgroundColor.svelte
+++ src/components/LayoutBackgroundColor.svelte
@@ -7,13 +7,11 @@
 	const backgroundsForPath = {
 		// these values correspond to the html background colors
 		// defined in each file
-		'/photos': 'var(--c2)',
-		'/rsvp': 'var(--c2)',
-		'/attendees': 'var(--c3)',
-		'/registry': 'var(--c4)'
+		photos: 'var(--c2)',
+		registry: 'var(--c4)'
 	};
 
-	$: backgroundColorValue = backgroundsForPath[path] ?? 'var(--c1)';
+	$: backgroundColorValue = backgroundsForPath[path.split('/')[1]] ?? 'var(--c1)';
 	$: pageTransitionDurationValue = `${pageTransitionDuration}ms`;
 
 	$: if (typeof window !== 'undefined') {
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: {
+				plugins: [autoprefixer]
+			}
+		})
+	],
 
 	kit: {
-		target: '#svelte',
-		adapter: adapter()
+		adapter: adapter(),
+		target: '#svelte'
 	}
 };
 
